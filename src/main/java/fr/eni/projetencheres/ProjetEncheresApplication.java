7f99d00b56bff7bc018a065ced3da1b2628e0d2a
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-01-22 14:34:55 +0100

    Initial commit

---
+++ src/main/java/fr/eni/projetencheres/ProjetEncheresApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.projetencheres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEncheresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetEncheresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/eni/projetencheres/ProjetEncheresApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.projetencheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEncheresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
