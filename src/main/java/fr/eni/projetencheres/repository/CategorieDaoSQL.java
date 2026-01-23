e7743a71b89ffb843b5f4d20fb3f6b6dbc439096
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-01-23 16:11:06 +0100

    no SQL profile, we only use SQL

--- src/main/java/fr/eni/projetencheres/repository/CategorieDaoSQL.java
+++ src/main/java/fr/eni/projetencheres/repository/CategorieDaoSQL.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-@Profile("SQL")
 public class CategorieDaoSQL implements CategorieDao{
 
     JdbcTemplate jdbcTemplate;
