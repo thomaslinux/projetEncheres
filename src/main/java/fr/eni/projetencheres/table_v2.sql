e4f338fea441bfea2c3d40351d20f86ddc20a36b
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-01-26 13:45:55 +0100

    VARCHAR(255) as minimum VARCHAR size everywhere

--- src/main/java/fr/eni/projetencheres/table_v2.sql
+++ src/main/java/fr/eni/projetencheres/table_v2.sql
@@ -14,13 +14,13 @@ DROP TABLE IF EXISTS CATEGORIE;
 
 CREATE TABLE CATEGORIE (
 	id_categorie 	INTEGER IDENTITY(1,1) 	NOT NULL,
-    libelle        	VARCHAR(30) 			NOT NULL,
+    libelle        	VARCHAR(255) 			NOT NULL,
 	CONSTRAINT CATEGORIE_PK PRIMARY KEY (id_categorie)
 );
 
 CREATE TABLE ARTICLE (
 	id_article 			INTEGER IDENTITY(1,1) NOT NULL,
-	nom_article 		VARCHAR(30) 	NOT NULL,
+	nom_article 		VARCHAR(255) 	NOT NULL,
 	description 		VARCHAR(300) 	NOT NULL,
 	date_debut_enchere 	DATETIME2 		NOT NULL,
 	date_fin_enchere 	DATETIME2		NOT NULL,
@@ -37,15 +37,15 @@ ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 CREATE TABLE UTILISATEUR (
     id_utilisateur   INTEGER IDENTITY(1,1) NOT NULL,
-    pseudo           VARCHAR(30) NOT NULL,
-    nom              VARCHAR(30) NOT NULL,
-    prenom           VARCHAR(30) NOT NULL,
-    email            VARCHAR(20) NOT NULL,
-	password		 VARCHAR(30) NOT NULL,
-    telephone        VARCHAR(15),
-    rue              VARCHAR(30) NOT NULL,
-    code_postal      VARCHAR(10) NOT NULL,
-    ville            VARCHAR(30) NOT NULL,
+    pseudo           VARCHAR(255) NOT NULL,
+    nom              VARCHAR(255) NOT NULL,
+    prenom           VARCHAR(255) NOT NULL,
+    email            VARCHAR(255) NOT NULL,
+	password		 VARCHAR(255) NOT NULL,
+    telephone        VARCHAR(255),
+    rue              VARCHAR(255) NOT NULL,
+    code_postal      VARCHAR(255) NOT NULL,
+    ville            VARCHAR(255) NOT NULL,
     credit           INTEGER 	 NOT NULL,
     administrateur   bit 		 NOT NULL,
 	actif			 bit 		 NOT NULL,
@@ -54,7 +54,7 @@ CREATE TABLE UTILISATEUR (
 
 CREATE TABLE ROLE (
     id_role 		INTEGER 	IDENTITY (1,1) 	NOT NULL,
-    role 			VARCHAR(30) NOT NULL,
+    role 			VARCHAR(255) NOT NULL,
 	id_utilisateur INTEGER 		NOT NULL,
     CONSTRAINT ROLE_PK PRIMARY KEY (id_role)
 );
@@ -84,9 +84,9 @@ ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 CREATE TABLE RETRAIT (
 	id_retrait 			INTEGER 	IDENTITY(1,1) NOT NULL,
-	rue              	VARCHAR(30) NOT NULL,
-    code_postal      	VARCHAR(15) NOT NULL,
-    ville            	VARCHAR(30) NOT NULL,
+	rue              	VARCHAR(255) NOT NULL,
+    code_postal      	VARCHAR(255) NOT NULL,
+    ville            	VARCHAR(255) NOT NULL,
 	id_article			INTEGER 	NOT NULL,
 	CONSTRAINT RETRAIT_PK PRIMARY KEY (id_retrait)
 );
