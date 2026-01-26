6e9570c10799f8a7fba68bcfb4941f6b5818a2c3
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-01-26 16:40:07 +0100

    removed unused imports, anonymised the test user

--- src/test/java/fr/eni/projetencheres/EnchereDaoTests.java
+++ src/test/java/fr/eni/projetencheres/EnchereDaoTests.java
@@ -1,17 +1,9 @@
 package fr.eni.projetencheres;
 
-import fr.eni.projetencheres.bo.Article;
-import fr.eni.projetencheres.bo.Categorie;
-import fr.eni.projetencheres.bo.Enchere;
-import fr.eni.projetencheres.bo.Utilisateur;
-import fr.eni.projetencheres.repository.CategorieDao;
 import fr.eni.projetencheres.repository.EnchereDao;
-import fr.eni.projetencheres.repository.UtilisateurDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDate;
 
 @SpringBootTest
 public class EnchereDaoTests {
@@ -26,10 +18,10 @@ public class EnchereDaoTests {
 
 //    @Test
 //    void testAddEnchere() {
-////        Utilisateur utilisateur = new Utilisateur("thomaslinux","linux","thomas",
-////                "thomaslinux@noreply.github.com","eafeafg465465!",
+////        Utilisateur utilisateur = new Utilisateur("anonymousMaster","master","anonymous",
+////                "anonymousmaster@noreply.github.com","eafeafg465465!",
 ////                "0606060606","rue de la rue",
-////                "35000","Rennes",699,false,true);
+////                "69000","New York",699,false,true);
 ////        utilisateurDao.addUtilisateur(utilisateur);
 ////        Categorie categorie = new Categorie("Ordinateur");
 ////        Article article = new Article("nom_article","description",
@@ -44,17 +36,17 @@ public class EnchereDaoTests {
 ////        System.out.println(enchere);
 //    }
 
-//    @Test
-//    void testDeleteEnchere() {
-////        Enchere enchere = new Enchere("Tableau");
-////        enchereDao.addEnchere(enchere);
-////        enchereDao.deleteEnchere(enchere.getId_enchere());
-////        System.out.println("Enchere list after delete");
-////        enchereDao.readEncheres().forEach(System.out::println);
-//    }
-//
-//    @Test
-//    void testUpdateEnchere() {
-////        enchereDao.updateEnchere(new Enchere(1,"Informatique"));
-//    }
+    @Test
+    void testDeleteEnchere() {
+//        Enchere enchere = new Enchere("Tableau");
+//        enchereDao.addEnchere(enchere);
+//        enchereDao.deleteEnchere(enchere.getId_enchere());
+//        System.out.println("Enchere list after delete");
+//        enchereDao.readEncheres().forEach(System.out::println);
+    }
+
+    @Test
+    void testUpdateEnchere() {
+//        enchereDao.updateEnchere(new Enchere(1,"Informatique"));
+    }
 }
