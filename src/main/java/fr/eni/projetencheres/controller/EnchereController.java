654c2b67dcd691f5c29444dbc0142dc39d650c73
silviabamas <silvia.bamas2025@campus-eni.fr>
2026-01-26 16:55:16 +0100

    modif sur HTML et controller

--- src/main/java/fr/eni/projetencheres/controller/EnchereController.java
+++ src/main/java/fr/eni/projetencheres/controller/EnchereController.java
@@ -1,17 +1,54 @@
 package fr.eni.projetencheres.controller;
 
 
+import fr.eni.projetencheres.service.UtilisateurService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping({"/encheres"})
+
 @Controller
+@RequestMapping("/encheres")
+
 public class EnchereController {
+
+    @Autowired
+    UtilisateurService utilisateurService;
 
     @GetMapping
     public String pageDAccueil() {
         return "index";
+    }
+
+    @GetMapping ("/inscription")
+    public String inscription() {
+        return "s_inscrire";
+    }
+
+    @GetMapping ("/vendre")
+    public String Vendre() {
+        return "add_vente";
+    }
+
+    @GetMapping ("/details_vente")
+    public String details() {
+        return "details_vente";
+    }
+
+    @GetMapping ("/liste_des_artVente")
+    public String liste_des_artVente() {
+        return "liste_des_artVente";
+    }
+
+    @GetMapping ("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping ("/profil")
+    public String profil() {
+        return "profil";
     }
 
 }
