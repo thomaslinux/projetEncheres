65df598e5fcba0b4f6ff03e3e7cdc207a7bedc8e
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-01-27 14:58:31 +0100

    cree utilisateur si tous les champs sont valides

--- src/main/java/fr/eni/projetencheres/controller/LoginController.java
+++ src/main/java/fr/eni/projetencheres/controller/LoginController.java
@@ -2,6 +2,7 @@ package fr.eni.projetencheres.controller;
 
 
 import fr.eni.projetencheres.bo.Utilisateur;
+import fr.eni.projetencheres.repository.UtilisateurDao;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
+
+    UtilisateurDao utilisateurDao;
+
+    public LoginController(UtilisateurDao utilisateurDao) {
+        this.utilisateurDao = utilisateurDao;
+    }
+
     @GetMapping({"/login"})
     public String displayLogin() {
         return "login";
@@ -26,9 +34,10 @@ public class LoginController {
       @PostMapping ("/inscription")
     public String inscription(@Valid Utilisateur utilisateur, BindingResult result, Model model) {
         if (result.hasErrors()) {
-            return "signup"; // affiche les erreurs sur la page
+            return "signup"; // renvoie la page avec les erreurs affichées
         }
-        // save the utilisateur logic here
+        utilisateurDao.addUtilisateur(utilisateur);
+          System.out.println(utilisateur);
         return "redirect:/login";
       }
 
