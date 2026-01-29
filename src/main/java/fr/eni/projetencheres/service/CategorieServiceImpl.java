46271aff94c0b39fe9762ebe2bd4be7dfcfa3f9b
silviabamas <silvia.bamas2025@campus-eni.fr>
2026-01-29 11:52:29 +0100

    Page Add Article en Get

---
+++ src/main/java/fr/eni/projetencheres/service/CategorieServiceImpl.java
@@ -0,0 +1,45 @@
+package fr.eni.projetencheres.service;
+
+
+import fr.eni.projetencheres.bo.Categorie;
+import fr.eni.projetencheres.repository.CategorieDao;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategorieServiceImpl implements CategorieService  {
+    CategorieDao categorieDao;
+
+
+    public CategorieServiceImpl(CategorieDao categorieDao) {
+        this.categorieDao = categorieDao;
+    }
+
+    @Override
+    public List<Categorie> getAllCategories() {
+        return this.categorieDao.readCategories();
+    }
+
+    @Override
+    public void addCategoeire(Categorie categorie) {
+        this.categorieDao.addCategorie(categorie);
+
+    }
+
+    @Override
+    public Categorie getCategorie(long id) {
+        return this.categorieDao.getCategorie(id);
+    }
+
+    @Override
+    public void deleteCategorie(long id) {
+        this.categorieDao.deleteCategorie(id);
+
+    }
+
+    @Override
+    public void updateCategorie(Categorie categorie) {
+
+    }
+}
