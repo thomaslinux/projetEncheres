7d992ff31a09e4fdba8951720e48db4026147187
Lenn-a <lena.morfoisse2025@campus-eni.fr>
2026-02-03 10:03:24 +0100

    sql

--- src/valeurs_test.sql
+++ src/valeurs_test.sql
@@ -1,3 +1,9 @@
+
+INSERT INTO UTILISATEUR(pseudo, nom, prenom, email, password, telephone, adresse, code_postal, ville, credit, administrateur, actif)
+VALUES ('SuperVendeur', 'Super', 'Vendeur', 'supervendeur@vente.com', 'JADOREVENDRE', '06 66 66 66 66', '15 rue des Vendeurs', '50000', 'VenteVille', 500, 0, 1);
+
+
+
 INSERT INTO CATEGORIE( libelle)
 VALUES  ('INFORMATIQUE' ),
         ('SPORT & LOISIRS'),
@@ -5,14 +11,15 @@ VALUES  ('INFORMATIQUE' ),
         ('VÊTEMENT');
 
 
-INSERT INTO ARTICLE (nom_article,description, date_debut_enchere, date_fin_enchere, prix_de_base, prix_de_vente, vente_en_cours, id_categorie, image_lien)
+INSERT INTO ARTICLE (nom_article,description, date_debut_enchere, date_fin_enchere, prix_de_base, prix_de_vente, vente_en_cours, id_categorie, image_lien, id_utilisateur)
 VALUES
-    ('Ballon', 'Ballon signe par Kylian Mbappé', null, null, '100', null, 1, 2, 'ballonKilyan.jpg'),
-    ('Canapé', 'Canapé 3 places en cuir', NULL, NULL,'450', NULL, 1, 3, 'canape.jpg'),
-    ('Table basse', 'Table basse en bois massif', NULL, NULL, '180', NULL, 1, 3, 'table_basse.jpg'),
-    ('Veste', 'Veste en jean taille M', NULL, NULL, '60', NULL, 1, 4, 'veste.jpg'),
-    ('Ordinateur portable', 'PC portable i5 16Go RAM', NULL, NULL, '750', NULL, 1, 1, 'ordinateur_portable.jpg'),
-    ('Écran', 'Écran 27 pouces Full HD', NULL, NULL, '220', NULL, 1, 1, 'ecran.jpg');
+    ('Ballon', 'Ballon signe par Kylian Mbappé', null, null, '100', null, 1, 2, 'ballonKilyan.jpg', 1),
+    ('Canapé', 'Canapé 3 places en cuir', NULL, NULL,'450', NULL, 1, 3, 'canape.jpg', 1),
+    ('Table basse', 'Table basse en bois massif', NULL, NULL, '180', NULL, 1, 3, 'table_basse.jpg', 1),
+    ('Veste', 'Veste en jean taille M', NULL, NULL, '60', NULL, 1, 4, 'veste.jpg', 1),
+    ('Ordinateur portable', 'PC portable i5 16Go RAM', NULL, NULL, '750', NULL, 1, 1, 'ordinateur_portable.jpg', 1),
+    ('Écran', 'Écran 27 pouces Full HD', NULL, NULL, '220', NULL, 1, 1, 'ecran.jpg', 1);
+
 SELECT * FROM ARTICLE
 SELECT * FROM CATEGORIE
 
