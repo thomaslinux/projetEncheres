dde017b558669b85a132a00095fee76910e99d41
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-05 13:28:15 +0100

    enleve les unused imports

--- src/main/java/fr/eni/projetencheres/controller/EnchereController.java
+++ src/main/java/fr/eni/projetencheres/controller/EnchereController.java
@@ -10,20 +10,16 @@ import fr.eni.projetencheres.service.ArticleService;
 import fr.eni.projetencheres.service.CategorieService;
 import fr.eni.projetencheres.service.EnchereService;
 import fr.eni.projetencheres.service.UtilisateurService;
-import jakarta.validation.Valid;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
